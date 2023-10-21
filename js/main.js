f84bcf667b3a8676b5a832bb43fdf511b1e30840
Harry-Rajput <146533634+Harry-Rajput@users.noreply.github.com>
2023-10-21 23:25:43 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -323,58 +323,58 @@
     };
 
 
-   /* Contact Form
-    * ------------------------------------------------------ */
-    var clContactForm = function() {
-        
-        /* local validation */
-        $('#contactForm').validate({
-        
-            /* submit via ajax */
-            submitHandler: function(form) {
+  /* Contact Form
+ * ------------------------------------------------------ */
+var clContactForm = function() {
     
-                var sLoader = $('.submit-loader');
+    /* local validation */
+    $('#contactForm').validate({
     
-                $.ajax({
-    
-                    type: "POST",
-                    url: "inc/sendEmail.php",
-                    data: $(form).serialize(),
-                    beforeSend: function() { 
-    
-                        sLoader.slideDown("slow");
-    
-                    },
-                    success: function(msg) {
-    
-                        // Message was sent
-                        if (msg == 'OK') {
-                            sLoader.slideUp("slow"); 
-                            $('.message-warning').fadeOut();
-                            $('#contactForm').fadeOut();
-                            $('.message-success').fadeIn();
-                        }
-                        // There was an error
-                        else {
-                            sLoader.slideUp("slow"); 
-                            $('.message-warning').html(msg);
-                            $('.message-warning').slideDown("slow");
-                        }
-    
-                    },
-                    error: function() {
-    
+        /* submit via ajax */
+        submitHandler: function(form) {
+
+            var sLoader = $('.submit-loader');
+
+            $.ajax({
+
+                type: "POST",
+                url: "https://formsubmit.co/harry@nawke.com",
+                data: $(form).serialize(),
+                beforeSend: function() { 
+
+                    sLoader.slideDown("slow");
+
+                },
+                success: function(response) {
+
+                    // Check if message was sent
+                    if (response.success) {
                         sLoader.slideUp("slow"); 
-                        $('.message-warning').html("Something went wrong. Please try again.");
-                        $('.message-warning').slideDown("slow");
-    
+                        $('.message-warning').fadeOut();
+                        $('#contactForm').fadeOut();
+                        $('.message-success').fadeIn();
                     }
-    
-                });
-            }
-    
-        });
-    };
+                    // There was an error
+                    else {
+                        sLoader.slideUp("slow"); 
+                        $('.message-warning').html(response.message);
+                        $('.message-warning').slideDown("slow");
+                    }
+
+                },
+                error: function() {
+
+                    sLoader.slideUp("slow"); 
+                    $('.message-warning').html("Something went wrong. Please try again.");
+                    $('.message-warning').slideDown("slow");
+
+                }
+
+            });
+        }
+
+    });
+};
 
 
    /* Animate On Scroll
